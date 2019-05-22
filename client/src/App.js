a8525bc48029a18d4e61f7de4fd27c8d4db3e0d8
JEFFREY <hoipck@gmail.com>
2019-05-22 02:01:48 +0000

    update

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,10 @@ import "./App.css";
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = { 
+            apiResponse: "",
+            dbResponse: "" 
+        };
     }
 
     callAPI() {
@@ -15,8 +18,16 @@ class App extends Component {
             .catch(err => err);
     }
 
+    callDB() {
+        fetch("http://115.146.92.40:9000/testDB")
+            .then(res => res.text())
+            .then(res => this.setState({ dbResponse: res }))
+            .catch(err => err);
+    }
+
     componentDidMount() {
         this.callAPI();
+        this.callDB();
     }
 
     render() {
@@ -37,6 +48,7 @@ class App extends Component {
                     </a>
                 </header>
                 <p className="App-intro">{this.state.apiResponse}</p>
+                <p className="App-intro">{this.state.dbResponse}</p>
             </div>
         );
     }
