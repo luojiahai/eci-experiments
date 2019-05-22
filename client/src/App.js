f63601471d5c1d9ee87ff1296edbea55f5aff478
JEFFREY <hoipck@gmail.com>
2019-05-22 04:57:36 +0000

    update

--- client/src/App.js
+++ client/src/App.js
@@ -169,7 +169,7 @@ class App extends Component {
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h2>ECI Experiments</h2>
+                    <h2>ECI-EXPERIMENTS</h2>
                 </div>
                 {this.state.result ? this.renderResult() : this.renderQuiz()}
             </div>
