5f2d9144d043fdf13a5b655d59558f115fe1ba28
JEFFREY <hoipck@gmail.com>
2019-05-22 04:35:49 +0000

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Quiz from './components/Quiz';
+import Result from './components/Result';
+import logo from "./svg/logo.svg";
 import "./App.css";
 
+import quizQuestions from './tempAPI/quizQuestions';
+
 class App extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { 
+    //         apiResponse: "",
+    //         dbResponse: "" 
+    //     };
+    // }
+    
     constructor(props) {
         super(props);
-        this.state = { 
-            apiResponse: "",
-            dbResponse: "" 
+      
+        this.state = {
+            counter: 0,
+            questionId: 1,
+            question: '',
+            answerOptions: [],
+            answer: '',
+            answersCount: {},
+            result: ''
         };
+
+        this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
     }
 
     callAPI() {
@@ -25,30 +45,133 @@ class App extends Component {
             .catch(err => err);
     }
 
+    // componentDidMount() {
+    //     this.callAPI();
+    //     this.callDB();
+    // }
+
     componentDidMount() {
-        this.callAPI();
-        this.callDB();
+        const shuffledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));  
+      
+        this.setState({
+            question: quizQuestions[0].question,
+            answerOptions: shuffledAnswerOptions[0]
+        });
+    }
+
+    shuffleArray(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+      
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+      
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+        
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+      
+        return array;
+    };
+
+    handleAnswerSelected(event) {
+        this.setUserAnswer(event.currentTarget.value);
+        if (this.state.questionId < quizQuestions.length) {
+            setTimeout(() => this.setNextQuestion(), 300);
+        } else {
+            setTimeout(() => this.setResults(this.getResults()), 300);
+        }
+    }
+
+    setUserAnswer(answer) {
+        this.setState((state) => ({
+            answersCount: {
+                ...state.answersCount,
+                [answer]: (state.answersCount[answer] || 0) + 1
+            },
+            answer: answer
+        }));
+    }
+
+    setNextQuestion() {
+        const counter = this.state.counter + 1;
+        const questionId = this.state.questionId + 1;
+        this.setState({
+            counter: counter,
+            questionId: questionId,
+            question: quizQuestions[counter].question,
+            answerOptions: quizQuestions[counter].answers,
+            answer: ''
+        });
+    }
+
+    getResults() {
+        const answersCount = this.state.answersCount;
+        const answersCountKeys = Object.keys(answersCount);
+        const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
+        const maxAnswerCount = Math.max.apply(null, answersCountValues);
+      
+        return answersCountKeys.filter((key) => answersCount[key] === maxAnswerCount);
+    }
+
+    setResults (result) {
+        if (result.length === 1) {
+            this.setState({ result: result[0] });
+        } else {
+            this.setState({ result: 'Undetermined' });
+        }
+    }
+
+    renderQuiz() {
+        return (
+            <Quiz
+                answer={this.state.answer}
+                answerOptions={this.state.answerOptions}
+                questionId={this.state.questionId}
+                question={this.state.question}
+                questionTotal={quizQuestions.length}
+                onAnswerSelected={this.handleAnswerSelected}
+            />
+        );
+    }
+      
+    renderResult() {
+        return (
+            <Result quizResult={this.state.result} />
+        );
     }
 
     render() {
         return (
+            // <div className="App">
+            //     <header className="App-header">
+            //         <img src={logo} className="App-logo" alt="logo" />
+            //         <p>
+            //             Edit <code>src/App.js</code> and save to reload.
+            //         </p>
+            //         <a
+            //         className="App-link"
+            //         href="https://reactjs.org"
+            //         target="_blank"
+            //         rel="noopener noreferrer"
+            //         >
+            //             Learn React
+            //         </a>
+            //     </header>
+            //     <p className="App-intro">{this.state.apiResponse}</p>
+            //     <p className="App-intro">{this.state.dbResponse}</p>
+            // </div>
+            
             <div className="App">
-                <header className="App-header">
+                <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
-                <p className="App-intro">{this.state.dbResponse}</p>
+                    <h2>ECI Experiments</h2>
+                </div>
+                {this.state.result ? this.renderResult() : this.renderQuiz()}
             </div>
         );
     }
