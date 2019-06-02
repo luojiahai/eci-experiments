6f057d21e67e1744b99f7151df651f0ac75c79b1
JEFFREY <hoipck@gmail.com>
2019-06-02 04:46:18 +0000

    update

--- client/src/tempAPI/dataset.js
+++ client/src/tempAPI/dataset.js
@@ -5,12 +5,12 @@ var dataset = {
     ],
     attributeNames: [
         'Age',
-        'Workclass',
+        // 'Workclass',
         'Education',
-        'Marital Status',
+        // 'Marital Status',
         'Occupation',
-        'Relationship',
-        'Race',
+        // 'Relationship',
+        // 'Race',
         'Sex',
         'Hours per week',
         'Native Country'
@@ -22,36 +22,36 @@ var dataset = {
             '37.00 < Age <= 48.00',
             'Age > 48.00'
         ],
-        [
-            '?', 'Federal-gov', 'Local-gov', 'Never-worked', 'Private',
-            'Self-emp-inc', 'Self-emp-not-inc', 'State-gov', 'Without-pay'
-        ],
+        // [
+        //     '?', 'Federal-gov', 'Local-gov', 'Never-worked', 'Private',
+        //     'Self-emp-inc', 'Self-emp-not-inc', 'State-gov', 'Without-pay'
+        // ],
         [
             '10th', '11th', '12th', '1st-4th', '5th-6th', '7th-8th',
             '9th', 'Assoc-acdm', 'Assoc-voc', 'Bachelors', 'Doctorate',
             'HS-grad', 'Masters', 'Preschool', 'Prof-school',
             'Some-college'
         ],
+        // [
+        //     'Divorced', 'Married-AF-spouse', 'Married-civ-spouse',
+        //     'Married-spouse-absent', 'Never-married', 'Separated',
+        //     'Widowed'
+        // ],
         [
-            'Divorced', 'Married-AF-spouse', 'Married-civ-spouse',
-            'Married-spouse-absent', 'Never-married', 'Separated',
-            'Widowed'
-        ],
-        [
-            '?', 'Adm-clerical', 'Armed-Forces', 'Craft-repair',
+            'Unknown', 'Adm-clerical', 'Armed-Forces', 'Craft-repair',
             'Exec-managerial', 'Farming-fishing', 'Handlers-cleaners',
             'Machine-op-inspct', 'Other-service', 'Priv-house-serv',
             'Prof-specialty', 'Protective-serv', 'Sales', 'Tech-support',
             'Transport-moving'
         ],
-        [
-            'Husband', 'Not-in-family', 'Other-relative', 'Own-child',
-            'Unmarried', 'Wife'
-        ],
-        [
-            'Amer-Indian-Eskimo', 'Asian-Pac-Islander', 'Black', 'Other',
-            'White'
-        ],
+        // [
+        //     'Husband', 'Not-in-family', 'Other-relative', 'Own-child',
+        //     'Unmarried', 'Wife'
+        // ],
+        // [
+        //     'Amer-Indian-Eskimo', 'Asian-Pac-Islander', 'Black', 'Other',
+        //     'White'
+        // ],
         [
             'Female', 'Male'
         ],
@@ -61,7 +61,7 @@ var dataset = {
             'Hours per week > 45.00'
         ],
         [
-            '?', 'Cambodia', 'Canada', 'China', 'Columbia', 'Cuba',
+            'Unknown', 'Cambodia', 'Canada', 'China', 'Columbia', 'Cuba',
             'Dominican-Republic', 'Ecuador', 'El-Salvador', 'England',
             'France', 'Germany', 'Greece', 'Guatemala', 'Haiti',
             'Holand-Netherlands', 'Honduras', 'Hong', 'Hungary', 'India',
